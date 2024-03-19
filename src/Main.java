04c46d5d8bf23cf6ec78d14137be9893a8c3d633
Nguyen Dac Phuong <dacphuongotp@gmail.com>
2024-03-20 02:21:25 +0700

    Update exercise

--- src/Main.java
+++ src/Main.java
@@ -2,71 +2,7 @@ import exercise.javabasic.*;
 
 public class Main {
     public static void main(String[] args) {
-        ExerciseBasic exerciseBasic = new ExerciseBasic();
-
-        /*
-        * 1. Viết chương trình nhập vào từ bàn phím một số nguyên n trong khoảng từ 1 đến 100. Sau đó kiểm tra:
-            - Nếu n là số lẻ, in ra màn hình "Odd"
-            - Nếu n là số chẵn trong khoảng từ 1 đến 30, in ra màn hình "Small Even"
-            - Nếu n là số chẵn trong khoảng từ 31 đến 60, in ra màn hình "Medium Even"
-            - Nếu n là số chẵn trong khoảng từ 61 đến 100, in ra màn hình "Big Even"
-            Yêu cầu: Chương trình chạy được và bắt hết các trường hợp ngoại lệ
-        * */
-//        exerciseBasic.importConsoleOne(); // Kết quả bài 1
-
-        /*
-        * 2. Viết chương trình nhập vào tên của 2 người, kiểm tra xem tên của 2 người này có giống nhau không.
-        * */
-//        exerciseBasic.importConsoleTwo(); // Kêt quả bài 2
-
-        /*
-         * Cho biến x và y kiểu số nguyên được nhập vào từ bàn phím (cả x và y đều khác 0)..
-         * Xác định điểm có tọa độ (x, y) đó nằm ở góc phần tư nào (Hình 1)
-         * */
-//        exerciseBasic.importConsoleThree(); // Kêt quả bài 3
-
-        /*
-         4. Viết chương trình hiển thị ra màn hình các số từ 0 tới 24 giống như sau:
-            0 1 2 3 4
-            5 6 7 8 9
-            10 11 12 13 14
-            15 16 17 18 19
-            20 21 22 23 24
-            Yêu cầu: Không in ra thủ công. VD:
-            print(0, 1, 2, 3, 4)
-            print(5, 6, 7, 8, 9)
-            ...
-         */
-//        exerciseBasic.importConsoleFour(); // Kêt quả bài 4
-
-        /*
-         * Viết chương trình hiển thị ra màn hình các số có số 0 ở cuối từ 1 tới 100 (màn hình sẽ hiển thị lên các số 10, 20, 30, ..., 90, 100)
-         * */
-//        exerciseBasic.importConsoleFive();// Kêt quả bài 5
-
-        /*
-         *6. Cho một mảng các số nguyên n phần tử được nhập từ bàn phím. Viết chương trình hiển thị ra:
-            - Phần tử lớn nhất trong mảng.
-            - Tổng của phần tử đầu tiên và cuối cùng trong mảng.
-            - Những số chẵn trong mảng.
-            - Tổng các số lẻ lớn hơn 0 trong mảng.
-            - Mảng được sắp xếp theo thứ tự tăng dần.
-         * */
-        exerciseBasic.importConsoleSix(); // Kêt quả bài 6
-
-        /*
-         *7. Cho một mảng 2 chiều các số nguyên với n hàng m cột (n, m và các phần tử trong mảng được nhập từ bàn phím). Viết chương trình tính:
-    - Tổng các phần tử trong mảng.
-    - Tổng các phần tử chia hết cho 5.
-         * */
-        // Kêt quả bài 7
-
-        /*
-         *8. Cho chuỗi s và ký tự c được nhập từ bàn phím. Viết chương trình in ra:
-    - Số lần xuất hiện của ký tự c trong chuỗi s.
-    - Vị trí đầu tiên mà ký tự c xuất hiện trong chuỗi s.
-    - Chuỗi s bị đảo ngược.
-         * */
-        // Kêt quả bài 8
+        Router router = new Router("one");
+        router.pathRedirection();
     }
 }
--- src/exercise/javabasic/ExerciseBasic.java
+++ src/exercise/javabasic/ExerciseBasic.java
@@ -1,5 +1,8 @@
 package exercise.javabasic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ExerciseBasic {
@@ -97,23 +100,110 @@ public class ExerciseBasic {
         System.out.print("Nhập số lượng phần tử tron mảng: ");
         valueLength = scanner.nextInt();
         int[] arrayNumber = new int[valueLength];
-//        int[] arrayNumberEven = new int[];
+        ArrayList<Integer> arrayNumberEven = new ArrayList<>(); // Mảng số chẵn
+        ArrayList<Integer> arrayNumberOdd = new ArrayList<>(); // Mảng số lẻ
+        int totalNunberOdd = 0;
+
 
         for (int index = 0; index < valueLength; index++){
             System.out.print("Nhập vào giá trị các phần tử.: ");
             arrayNumber[index] = scanner.nextInt();
         }
         int valueMax = 0;
-        int valueSum = 0;
         for (int index = 0; index < valueLength; index++){
             if (arrayNumber[index] > valueMax){
+                // Tìm số lớn nhất
                 valueMax = arrayNumber[index];
             }
             if (arrayNumber[index] % 2 == 0){
-                System.out.println(arrayNumber[index]);
+                // Tìm các số chẵn
+                arrayNumberEven.add(arrayNumber[index]);
+            }else if (arrayNumber[index] % 2 != 0 && arrayNumber[index] > 0){
+                // Tìm các số lẻ và tính tổng của các số lẻ đo'
+                arrayNumberOdd.add(arrayNumber[index]);
+                totalNunberOdd += arrayNumber[index];
             }
         }
-        System.out.println("Max.: " + valueMax);
-        System.out.println("Tổng số đầu + cuối có giá trị là :" + (arrayNumber[0] + arrayNumber[valueLength - 1]));
+        System.out.println("Max: " + valueMax);
+        System.out.println("Tổng số (đầu + cuối) có giá trị là :" + (arrayNumber[0] + arrayNumber[valueLength - 1]));
+        System.out.println("Mảng số chẵn:" + arrayNumberEven.toString());
+        System.out.println("Mảng số chẵn:" + arrayNumberOdd.toString());
+        System.out.println("Tổng các số lẻ lớn hơn 0 là: " + totalNunberOdd);
+
+        for (int index = 0; index < valueLength; index++){
+            for (int k = index + 1; k < valueLength; k++){
+                if (arrayNumber[index] > arrayNumber[k]){
+                    int temp = arrayNumber[index];
+                    arrayNumber[index] = arrayNumber[k];
+                    arrayNumber[k] = temp;
+                }
+            }
+        }
+        System.out.println("Mảng đã sắp xếp theo thứ tự tăng dần :" + Arrays.toString(arrayNumber));
+    }
+
+    public void importConsoleSeven(){
+        System.out.print("Nhập vào số hàng : ");
+        int row = scanner.nextInt();
+
+        System.out.print("Nhập vào số cột : ");
+        int column = scanner.nextInt();
+
+        int[][] arrayNumber = new int[row][column];
+
+        if(row <= 1){
+            this.importConsoleSeven();
+        }else {
+            for(int index = 0; index < row; index++){
+                for (int k = 0; k < column; k++){
+                    System.out.print("Nhập vào giá trị: ");
+                    int value = scanner.nextInt();
+                    arrayNumber[index][k] = value;
+                }
+            }
+            // Tính tổng của các phần tử trong mảng hai chiều
+            int total = 0;
+            int totalDivisible = 0;
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < column; j++) {
+                    total += arrayNumber[i][j];
+                    if (arrayNumber[i][j] % 5 == 0){
+                        totalDivisible += arrayNumber[i][j];
+                    }
+                }
+                System.out.println();
+            }
+
+            System.out.println("Tổng của các phần tử trong mảng hai chiều : " + total);
+
+            System.out.println("Tổng của các phần tử chia hết cho 5 trong mảng hai chiều : " + totalDivisible);
+        }
+
+    }
+
+    public void importConsoleEight(){
+        System.out.print("Nhập chuỗi String : ");
+        String strValue = scanner.nextLine();
+        int numberActiveC = 0;
+        int indexFirtsC = 0;
+        for (int index = 0; index < strValue.length(); index++){
+
+            if (strValue.charAt(index) == 'c'){
+                if (numberActiveC == 0){
+                    indexFirtsC = index;
+                    numberActiveC += 1;
+                }else {
+                    numberActiveC += 1;
+                }
+            }
+        }
+        System.out.println("Số lần xuất hiện của c là : " + numberActiveC);
+
+        System.out.println("Vị trí xuất hiện đầu tiên của C là : " + indexFirtsC);
+        String strValueSort = "";
+        for (int index = strValue.length() - 1; index > 0; index--){
+            strValueSort += strValue.charAt(index);
+        }
+        System.out.println("Mảng đã xắp xếp lại là : " + strValueSort);
     }
 }
